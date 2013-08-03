26048cccbb358aef13fff8008660d4663c8a2760
Aleksey 'LXj' Alekseyev <gotletter@gmail.com>
2013-08-04 00:47:08 +0300

    Fixed test

--- tests.py
+++ tests.py
@@ -1,14 +1,13 @@
-from tornado.testing import AsyncHttpTestCase
-
+from tornado.testing import AsyncHTTPTestCase
 
 from .main import application, SITE_NAME
 
 
-class HelloWorldTestCase(AsyncHttpTestCase):
+class HelloWorldTestCase(AsyncHTTPTestCase):
     def get_app(self):
         return application
 
     def test_index(self):
         self.http_client.fetch(self.get_url('/'), self.stop)
         response = self.wait()
-        self.assertIn(SITE_NAME, response.body)
+        self.assertIn(SITE_NAME, response.body.decode('UTF-8'))
