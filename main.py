da43b2419aa47dc6df99f808ebe819ed6fc6bab9
Aleksey 'LXj' Alekseyev <gotletter@gmail.com>
2013-08-03 23:18:23 +0300

    Better organization of files

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from jinja2 import Environment, FileSystemLoader
 SITE_NAME = 'Lair'
 ROOT = os.path.dirname(__file__)
 TEMPLATE_ROOT = os.path.join(ROOT, 'templates')
-STATIC_PATH = os.path.join(ROOT, 'static')
+STATIC_ROOT = os.path.join(ROOT, 'static')
+STATIC_PATH = '/static/'
 
 define("port", default=8000, help="run on the given port", type=int)
 define("debug", default=1, help="run in debug mode", type=int)
@@ -21,8 +22,9 @@ def render_template(template, **context):
     env = Environment(loader=loader)
     template = env.get_template(template)
     context.update({
-        'site_name': SITE_NAME,
-        })
+        'SITE_NAME': SITE_NAME,
+        'STATIC_PATH': STATIC_PATH,
+    })
     return template.render(**context)
 
 
@@ -36,7 +38,7 @@ def main():
     parse_command_line()
     application = tornado.web.Application([
         (r"/", MainHandler),
-    ], debug=options.debug, static_path=STATIC_PATH)
+    ], debug=options.debug, static_path=STATIC_ROOT)
     application.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
