b8190e478a8c17736e413e13edae1dae2d656c46
Aleksey 'LXj' Alekseyev <gotletter@gmail.com>
2013-08-17 21:24:53 +0300

    Switched from  to

--- lair/web/static/js/tweets.js
+++ lair/web/static/js/tweets.js
@@ -1,9 +1,16 @@
 'use strict';
 
-function TweetListCtrl($scope, $http) {
-    $http.get('/data/tweets/').success(function(data) {
-        $scope.tweets = data;
+angular.module('LairApp', ['TweetServices']);
+
+angular.module('TweetServices', ['ngResource'])
+    .factory('Tweets', function($resource) {
+        return $resource('/data/tweets/?last_id=:last_id', {}, {
+            query: {method: 'GET', params: {last_id:'all'}, isArray: true}
+        });
     });
+
+function TweetListCtrl($scope, $http, Tweets) {
+    $scope.tweets = Tweets.query();
 }
 
-TweetListCtrl.$inject = ['$scope', '$http']
+// TweetListCtrl.$inject = ['$scope', '$http', 'Tweets']
