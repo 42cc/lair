446a33890f17db418e0451da266b5e99bdfc6d1a
Aleksey 'LXj' Alekseyev <gotletter@gmail.com>
2013-08-18 22:53:18 +0300

    Added output to twitter listener

--- lair/backend/twitter_home.py
+++ lair/backend/twitter_home.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+import os
+
 from twitter import TwitterStream, OAuth
 from pymongo import MongoClient
-
 
 from ..settings import TWITTER, MONGO_CONNECTION, MONGO_DB
 
@@ -17,6 +18,7 @@ def get_twitter_stream():
 
 
 def main():
+    testing = os.environ.get('TESTING', '')
     mongo = MongoClient(**MONGO_CONNECTION)
     stream = get_twitter_stream()
     db = mongo[MONGO_DB]
@@ -24,6 +26,9 @@ def main():
         if "id" not in tweet:
             continue
         tweet["_id"] = tweet["id"]
+        if not testing:
+            print('({}) {}: {}'.format(
+                tweet['id'], tweet["user"]["screen_name"], tweet["text"]))
         db.twitter_home.insert(tweet)
 
 
